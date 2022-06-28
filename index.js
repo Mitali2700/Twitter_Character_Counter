55fccbe79c5e1b0b6dbfade633185dfcd774b793
Mitali Gupta <78539161+Mitali2700@users.noreply.github.com>
2022-06-28 16:45:20 +0000

    Cut down tweet into 140 characters

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 var tweet = prompt("Compose your tweet: ");
 var tweetCount= tweet.length;
 alert("You have written "+ tweetCount + "characters, you have " +(140-tweetCount)+" Characters remaining");
+alert("You have written "+ tweet.slice(tweet,140));
