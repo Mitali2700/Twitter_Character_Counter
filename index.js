6eaf1711233a5b49a79fec4a49b529426703d92d
Mitali Gupta <78539161+Mitali2700@users.noreply.github.com>
2022-06-28 16:49:01 +0000

    remove slicing functionality

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 var tweet = prompt("Compose your tweet: ");
 var tweetCount= tweet.length;
-alert("You have written "+ tweetCount + "characters, you have " +(140-tweetCount)+" Characters remaining");
-alert("You have written "+ tweet.slice(tweet,140));
+alert("You have written "+ tweetCount + "characters, you have " +(140-tweetCount)+" Characters remaining\n \n");
